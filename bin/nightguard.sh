924e56df4d1ef576fd554f455aaa411aa0f210a9
Terry <terry@metatooth.com>
2020-05-06 20:59:28 -0400

    Save S3 information to the cabinet.

--- bin/nightguard.sh
+++ bin/nightguard.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <INDIR>"
+if [ "$#" -ne 2 ]; then
+    echo "usage: nightguard.sh <indir> <key>"
+    echo
+    echo "  Executes process steps 200 & 300 for subdirectories"
+    echo "  of <indir>. Using the API <key>, will also store"
+    echo "  300 output on s3://metatooth-cabinet."
+    echo
+    echo "  for example, nightguard.sh ~/metaspace/Nightguard \\"
+    echo "                 2:8e09332323e586eab46a1a2b5ead12f5"
     exit
 fi
 
 INDIR=$1
+KEY=$2
 
 if [ ! -d "$INDIR" ]; then
    echo "$INDIR does not exist. Try again."
@@ -24,9 +32,10 @@ for d in $INDIR/*; do
 
     arr=($check)
 
-    #aws s3 cp $d/300/$bname.stl s3://metatooth-cabinet/$year/$month/${arr[0]}.stl
-    curl -v http://localhost:9393/assets \
-	 -H "Authorization: Metaspace-Token api_key=17:1d184d055f41a5b5402e7bc1ab45b85f" \
+    aws s3 cp $d/300/$bname.stl s3://metatooth-cabinet/$year/$month/${arr[0]}.stl
+    curl -i -v http://localhost:9393/assets \
+	 -H 'Content-Type: application/json' \
+	 -H 'Authorization: Metaspace-Token api_key='$KEY \
 	 -d '{"data":{"name":"'$bname'","url":"https://metatooth-cabinet.s3.amazonaws.com/'$year'/'$month'/'${arr[0]}'.stl","mime_type":"application/sla"}}'
 done
 
