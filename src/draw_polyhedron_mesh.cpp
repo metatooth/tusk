7a03857176872a877f4ba65b5d822bb09c423beb
Terry G Lorber <terry@metatooth.com>
2020-09-30 11:58:56 -0400

    working

--- src/draw_polyhedron_mesh.cpp
+++ src/draw_polyhedron_mesh.cpp
@@ -18,46 +18,75 @@ using Polyhedron = CGAL::Polyhedron_3<K, CGAL::Polyhedron_items_with_id_3>;
 
 namespace PMP = CGAL::Polygon_mesh_processing;
 
-int main(int argc, char* argv[])
+void read_polyhedron_mesh_from_stl(std::istream& in, Polyhedron* out)
 {
-  Polyhedron polyhedron;
-
-  const char* filename = (argc > 1) ? argv[1] : "badfile";
-
-  std::ifstream input(filename, std::ios::binary);
   std::vector< CGAL::cpp11::array<double,3> > in_points;
-  std::vector< CGAL::cpp11::array<int,3> > triangles;
+  std::vector< CGAL::cpp11::array<size_t,3> > triangles;
 
-  CGAL::read_STL(input, in_points, triangles);
-
-  input.close();
+  CGAL::read_STL(in, in_points, triangles, false);
 
   std::cout << "points: " << in_points.size() << "\n"
             << "triangles: " << triangles.size() << std::endl;
 
   std::vector<K::Point_3> points;
-  for (CGAL::cpp11::array<double,3> p : in_points) {
-    points.push_back(K::Point_3(p[0], p[1], p[2]));
+  points.resize(in_points.size());
+
+  for (std::size_t i = 0, l = in_points.size(); i < l; i++) {
+    points[i] = K::Point_3(in_points[i][0], in_points[i][1], in_points[i][2]);
   }
 
   std::cout << "num points: " << points.size() << std::endl;
 
   std::vector<std::vector<std::size_t> > polygons;
+  polygons.resize(triangles.size());
 
-  for (CGAL::cpp11::array<int,3> t : triangles) {
-    std::vector<std::size_t> p = {(std::size_t) t[0],
-                                  (std::size_t) t[1],
-                                  (std::size_t) t[2]};
-    polygons.push_back(p);
+  for (std::size_t i = 0, l = triangles.size(); i < l; i++) {
+    polygons[i] = { triangles[i][0], triangles[i][1], triangles[i][2] };
   } 
 
   std::cout << "num polygons: " << polygons.size() << std::endl;
 
   PMP::orient_polygon_soup(points, polygons);
 
-  Polyhedron m;
-  PMP::polygon_soup_to_polygon_mesh(points, polygons, m);
+  std::cout << "Oriented." << std::endl;
 
-  CGAL::draw(m);
-  return EXIT_SUCCESS;
+  PMP::polygon_soup_to_polygon_mesh(points, polygons, *out);
+
+  std::cout << "Soup to mesh." << std::endl;
+}
+
+void usage()
+{
+  std::cerr << "mouthguard [infile] [fit] [thickness] [outfile]\n\n"
+            << "  Creates a mesh for a mouthguard for [infile] saving\n"
+            << "  in STL format to [outfile]. Mouthguard will have a\n"
+            << "  [thickness] specified in millimeters with an offset\n"
+            << "  of [fit] from the [infile] surface, also in millimeters.\n\n"
+            << "  for example, mouthguard 0000.stl 0000-design.stl"
+            << std::endl;
+}
+
+const std::size_t NUM_ARGS = 5;
+
+int main(int argc, char* argv[]) {
+
+  if (argc != NUM_ARGS) {
+    usage();
+    exit(-1);
+  }
+
+  std::ifstream ifs(argv[1], std::ios::binary);
+
+  Polyhedron res;
+  try {
+    read_polyhedron_mesh_from_stl(ifs, &res);
+  } catch (std::exception& e) {
+    std::cerr << "ERROR " << &e << std::endl;
+  }
+
+  ifs.close();
+
+  CGAL::draw(res);
+
+  return 0;
 }
